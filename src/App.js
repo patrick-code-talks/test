21beaebb01ff03ab049d5f3d1a43095097f7162a
patrick-code-talks <patrickcodes1995@gmail.com>
2022-12-02 11:36:16 -0800

    2

--- src/App.js
+++ src/App.js
@@ -1,110 +1,162 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+import cx from 'classnames';
+
 const PROPERTY = {
-  like: {
-    defaultClassName : {
-      btn: 'like-button',
-      counterClassName: 'likes-counter'
-    },
-    likedClassName : {
-      btn: 'like-button liked'
-    }
-    
+  btnID: {
+    likeBtn: 'btn_like',
+    dislikeBtn: 'btn_dislike'
   },
-
-  dislike: {
-    defaultClassName: {
-      btn: 'dislike-button',
-      counterClassName: 'dislikes-counter'
-    },
-    dislikedClassName: {
-      btn: 'dislike-button'
-    }
-  }
+  
 }
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-    console.log("props = ", props);
+    // console.log(props);
     this.state = {
-      currentClick: null,
-      prevClick: null,
+      clicked: null,
       likeBtn: {
-        counter: 100,
-        btnClassName: PROPERTY.like.defaultClassName.btn,
-        counterClassName: PROPERTY.like.defaultClassName.counterClassName,
+        counter: 100, // default number
+        btnClass: 'like-button',  // default style
+        // counterSpanClass: 'likes-counter'
       },
-      disLikeBtn: {
-        counter: 25,
-        btnClassName: PROPERTY.dislike.defaultClassName.btn,
-        counterClassName: PROPERTY.dislike.defaultClassName.counterClassName,
-      },
-      className: 'like-button',
-      likeText: 'Like | '
+      dislikeBtn: {
+        counter: 25,  // default number
+        btnClass: 'dislike-button', // default style
+        // counterSpanClass: 'dislikes-counter'
+      }
     }
-
   }
 
-  handleClick = e => {
-    const { id } = e.currentTarget;
-    const { prevClick, currentClick } = this.state;
-    let newState = {
-      ...this.state
-    };
-    if (id === 'btn_like') {
-      if (!prevClick) {
-        newState.likeBtn = {
-          ...newState.likeBtn,
-          counter: newState.likeBtn.counter + 1,
-          btnClassName: PROPERTY.like.likedClassName.btn,
-        }
-      } else if (prevClick === 'btn_like') {
-        newState.likeBtn = {
-          ...newState.likeBtn,
-          counter: newState.likeBtn.counter - 1,
-          btnClassName: PROPERTY.like.defaultClassName.btn
-        }
-      } else if (prevClick === 'btn_dislike') {
-        newState = {
-          ...newState,
+  // handleClick = e => {
+  //   const { id } = e.currentTarget;
+  //   console.log('id = ', id);
+  // }
+
+  handleClickLikeBtn = () => {
+    const { clicked } = this.state;
+    if (!clicked) { // 1st click
+      this.setState(prevState => {
+        return {
+          ...prevState,
+          clicked: PROPERTY.btnID.likeBtn,
           likeBtn: {
-            counter: newState.likeBtn.counter + 1,
-            btnClassName: PROPERTY.like.likedClassName.btn,
-          },
-          disLikeBtn: {
-            counter: newState.disLikeBtn.counter - 1,
-            btnClassName: PROPERTY.dislike.defaultClassName.btn,
+            counter: prevState.likeBtn.counter + 1,
+            btnClass: cx('like-button', 'liked'),
           }
         }
-      }
-    } else if (id === 'btn_dislike') {
-      if (!prevClick) {
-        
-      } else if (prevClick === 'btn_like') {
-
-      } else if (prevClick === 'btn_dislike') {
-
-      }
+      });
+    } else if (clicked === PROPERTY.btnID.likeBtn) {  // click like that already been clicked
+      this.setState(prevState => {
+        return {
+          ...prevState,
+          clicked: PROPERTY.btnID.likeBtn,
+          likeBtn: {
+            counter: prevState.likeBtn.counter - 1,
+            btnClass: cx('like-button', ''),
+          }
+        }
+      });
+    } else if (clicked === PROPERTY.btnID.dislikeBtn) { // click like after clicking dislike
+      this.setState(prevState => {
+        return {
+          ...prevState,
+          clicked: PROPERTY.btnID.likeBtn,
+          likeBtn: {
+            counter: prevState.likeBtn.counter + 1,
+            btnClass: cx('like-button', 'liked'),
+          },
+          dislikeBtn: {
+            counter: prevState.dislikeBtn.counter - 1,
+            btnClass: cx('dislike-button', ''),
+          }
+        }
+      });
     }
   }
 
+  handleClickDislikeBtn = () => {
+    const { clicked } = this.state;
+    if (!clicked) { // 1st click
+      this.setState(prevState => {
+        return {
+          ...prevState,
+          clicked: PROPERTY.btnID.dislikeBtn,
+          dislikeBtn: {
+            counter: prevState.dislikeBtn.counter + 1,
+            btnClass: cx('dislike-button', 'disliked'),
+          }
+        }
+      });
+    } else if (clicked === PROPERTY.btnID.likeBtn) {  // click dislike after clicking like
+      this.setState(prevState => {
+        return {
+          ...prevState,
+          clicked: PROPERTY.btnID.dislikeBtn,
+          likeBtn: {
+            counter: prevState.likeBtn.counter - 1,
+            btnClass: cx('like-button', ''),
+          },
+          dislikeBtn: {
+            counter: prevState.dislikeBtn.counter + 1,
+            btnClass: cx('dislike-button', 'disliked'),
+          }
+        }
+      });
+    } else if (clicked === PROPERTY.btnID.dislikeBtn) { // click dislike after clicking dislike
+      this.setState(prevState => {
+        return {
+          ...prevState,
+          clicked: PROPERTY.btnID.dislikeBtn,
+          dislikeBtn: {
+            counter: prevState.dislikeBtn.counter - 1,
+            btnClass: cx('dislike-button', ''),
+          }
+        }
+      });
+    }
+  }
+
+  getSpanText = (text, number) => {
+    return `${text} | ${number}`
+  }
+  
   render() {
     return (
-      <div className="App">
-        <button id='btn_like' className={this.state.className} onClick={this.handleClick}>
-          <span>{this.state.likeText}</span>
-          <span className='likes-counter liked'>100</span>
-        </button>
-        <button id='btn_dislike' className={this.state.className}>
-          <span>Dislike | </span>
-          <span className='liked'>100</span>
-        </button>
-      </div>
+      <>
+        <div>
+          <button id={PROPERTY.btnID.likeBtn} className={this.state.likeBtn.btnClass}
+          onClick={this.handleClickLikeBtn}>
+            <span className='likes-counter'>
+              {this.getSpanText('Like', this.state.likeBtn.counter)}
+            </span>
+          </button>
+
+          <button id={PROPERTY.btnID.dislikeBtn} className={this.state.dislikeBtn.btnClass}
+          onClick={this.handleClickDislikeBtn}>
+            <span className='dislikes-counter'>
+              {this.getSpanText('Dislike', this.state.dislikeBtn.counter)}
+            </span>
+          </button>
+
+        </div>
+        <style>{`
+                      .like-button, .dislike-button {
+                          font-size: 1rem;
+                          padding: 5px 10px;
+                          color:   #585858;
+                      }
+
+                      .liked, .disliked {
+                          font-weight: bold;
+                          color: #1565c0;
+                      }
+                  `}</style>
+      </>
     );
   }
-}
+};
 
 export default App;
